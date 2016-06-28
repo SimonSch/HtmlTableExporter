f0482a79922c0d89b94370c86818db58be63f604
sschroffner <sschroffner@eurofunk.com>
2016-06-28 17:27:12 +0200

    Awesome export stuff with now 20x more awesome

--- src/stable/js/tableexporter.js
+++ src/stable/js/tableexporter.js
@@ -10,7 +10,7 @@
 
     /*--- GLOBALS ---*/
     var $ = window.jQuery;
-
+ 
     function createHeader(html, options){
         $.each(options, function(i, column){
             if ($.isPlainObject(column)){
